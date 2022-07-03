9d81daae63881ddf1030f3c6f5af67a89893e410
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2022-07-03 00:39:13 -0300

    Adicionar arquivos de projeto.

---
+++ Intrumentacao2/Migrations/20220703032720_alteracaoLogin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Intrumentacao2.Migrations
+{
+    public partial class alteracaoLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
